f44c1a1b6683477cc1ceb780c71d83ae84e4d198
Mike Klimek <mike.klimek.developer@gmail.com>
2020-09-30 02:02:41 +0200

    Add intent for notification click

--- app/src/main/java/de/klimek/compass/Notification.kt
+++ app/src/main/java/de/klimek/compass/Notification.kt
@@ -3,6 +3,9 @@ package de.klimek.compass
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.provider.Settings
 import androidx.core.app.NotificationCompat
 
 const val CHANNEL_ID = "COMPASS_CHANNEL"
@@ -16,5 +19,15 @@ fun TileService.channel() = NotificationChannel(
 
 fun TileService.notification(): Notification = NotificationCompat.Builder(this, CHANNEL_ID)
     .setSmallIcon(R.drawable.ic_arrow)
-    .setAutoCancel(true)
+    .setContentIntent(notificationClickIntent())
     .build()
+
+
+fun TileService.notificationClickIntent(): PendingIntent {
+    val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+        putExtra(Settings.EXTRA_APP_PACKAGE, BuildConfig.APPLICATION_ID)
+        addCategory(Intent.CATEGORY_DEFAULT)
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+    return PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+}
