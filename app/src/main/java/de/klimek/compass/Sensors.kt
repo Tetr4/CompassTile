d23129c42bc0bcab89d285e13a5e507db3fbde20
Mike Klimek <mike.klimek.developer@gmail.com>
2022-11-27 18:14:16 +0100

    Add support for landscape, reverse landscape, reverse portrait

--- app/src/main/java/de/klimek/compass/Sensors.kt
+++ app/src/main/java/de/klimek/compass/Sensors.kt
@@ -2,14 +2,29 @@ package de.klimek.compass
 
 import android.hardware.SensorEvent
 import android.hardware.SensorManager
+import android.view.Surface.ROTATION_0
+import android.view.Surface.ROTATION_180
+import android.view.Surface.ROTATION_270
+import android.view.Surface.ROTATION_90
 
-fun SensorEvent.getAzimuthDegrees(): Float {
-    val rotationMatrix = FloatArray(9)
-    SensorManager.getRotationMatrixFromVector(rotationMatrix, this.values)
-    val orientation = FloatArray(3)
-    SensorManager.getOrientation(rotationMatrix, orientation)
+
+fun SensorEvent.getAzimuthDegrees(displayRotation: Int?): Float {
+    val rotationMatrix = FloatArray(9).also {
+        SensorManager.getRotationMatrixFromVector(it, this.values)
+    }
+    val orientation = FloatArray(3).also {
+        SensorManager.getOrientation(rotationMatrix, it)
+    }
     val azimuthRadians = orientation.getOrElse(0) { 0f }
-    return Math.toDegrees(azimuthRadians.toDouble()).normalizeDegrees()
+    val azimuthDegrees = Math.toDegrees(azimuthRadians.toDouble())
+    val displayAdjustedDegrees = when (displayRotation) {
+        ROTATION_0 -> azimuthDegrees
+        ROTATION_90 -> azimuthDegrees + 90
+        ROTATION_180 -> azimuthDegrees + 180
+        ROTATION_270 -> azimuthDegrees + 270
+        else -> azimuthDegrees
+    }
+    return displayAdjustedDegrees.normalizeDegrees()
 }
 
 private fun Double.normalizeDegrees() = ((this + 360) % 360).toFloat()
--- app/src/main/java/de/klimek/compass/TileService.kt
+++ app/src/main/java/de/klimek/compass/TileService.kt
@@ -1,15 +1,17 @@
 package de.klimek.compass
 
 import android.app.NotificationManager
-import android.content.Context
 import android.graphics.drawable.Icon
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.hardware.display.DisplayManager
 import android.service.quicksettings.Tile
 import android.util.Log
+import android.view.Display
 import android.widget.Toast
+import androidx.core.content.getSystemService
 import de.klimek.compass.tile.IconFactory
 import de.klimek.compass.tile.label
 import de.klimek.compass.tile.update
@@ -20,28 +22,34 @@ private const val SENSOR_DELAY = SensorManager.SENSOR_DELAY_UI
 class TileService : android.service.quicksettings.TileService(), SensorEventListener {
 
     private val sensorManager
-        get() = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        get() = getSystemService<SensorManager>()
 
     private val notificationManager
-        get() = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        get() = getSystemService<NotificationManager>()
+
+    private val displayManager
+        get() = getSystemService<DisplayManager>()
 
     private val sensor by lazy {
         // TYPE_ROTATION_VECTOR is a fusion of gyro, accelerometer and magnetometer, which is more
         // accurate and responsive than just using the magnetometer.
         // TYPE_GEOMAGNETIC_ROTATION_VECTOR is used as a fallback if gyro is not available.
-        sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
-            ?: sensorManager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR)
+        sensorManager?.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+            ?: sensorManager?.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR)
     }
 
     private val isSupported
         get() = sensor != null
+
+    private val displayRotation
+        get() = displayManager?.getDisplay(Display.DEFAULT_DISPLAY)?.rotation
 
     private lateinit var iconFactory: IconFactory
 
     override fun onCreate() {
         Log.i(TAG, "Create")
         iconFactory = IconFactory(applicationContext, R.drawable.ic_qs_compass_on)
-        notificationManager.createNotificationChannel(channel())
+        notificationManager?.createNotificationChannel(channel())
     }
 
     override fun onStartListening() {
@@ -88,19 +96,19 @@ class TileService : android.service.quicksettings.TileService(), SensorEventList
     private fun startCompass() {
         Log.i(TAG, "Start")
         startForeground(NOTIFICATION_ID, notification())
-        sensorManager.registerListener(this, sensor, SENSOR_DELAY)
+        sensorManager?.registerListener(this, sensor, SENSOR_DELAY)
     }
 
     private fun stopCompass() {
         Log.i(TAG, "Stop")
-        sensorManager.unregisterListener(this)
-        stopForeground(true)
+        sensorManager?.unregisterListener(this)
+        stopForeground(STOP_FOREGROUND_REMOVE)
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) = Unit
 
     override fun onSensorChanged(event: SensorEvent) {
-        val degrees = event.getAzimuthDegrees()
+        val degrees = event.getAzimuthDegrees(displayRotation)
         Log.v(TAG, degrees.toString())
         qsTile.update {
             label = label(degrees)
